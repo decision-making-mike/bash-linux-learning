270a05867641890a848f05531491535abc30ef68
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2024-12-26 19:14:49 +0100

    Comment clarification and code cleanup

--- business-simulation/run.sh
+++ business-simulation/run.sh
@@ -4,7 +4,9 @@
 
 # Notable commands. "CTRL-D" ends the day without waiting, "CTRL-C" exits the simulation.
 
-# Passing variables as parameters. I avoid passing variables as parameters, either to functions or to commands. This is because I think it makes the code more complex, by which I mainly mean more difficult to follow. There are two consequences. First, I try not to define additional functions if possible, and second, I use more global variables.
+# Number of functions and commands used. The more functions and commands used, the harder to follow the code.
+
+# Global variables. If a variable is global, it makes it easier to follow it than when it's local.
 
 save_file_path="$1"
 if [[ -z "$save_file_path" ]]
@@ -93,42 +95,35 @@ min () {
     echo "$(( "$1" <= "$2" ? "$1" : "$2" ))"
 }
 
-handle_day () {
+get_total_gross_income () {
+    total_gross_income=0
     used_car_count="$(min "$driver_count" "$car_count")"
-    total_income=0
     if [[ "$manager_count" -gt 0 ]]
     then
         # I'm adding 1 to the maximum single manager vehicle count since this count is the maximum one that should make the manager yield at least minimal result. If the 1 weren't added, the manager would yield a result equal to zero.
         fleet_management_efficiency_rate_numerator="$(( "$maximum_single_manager_vehicle_count" + 1 - "$car_count" / "$manager_count" ))"
         fleet_management_efficiency_rate_denominator="$(( "$maximum_single_manager_vehicle_count" + 1 ))"
-        total_income="$(( "$fleet_management_efficiency_rate_numerator" * "$income" * "$used_car_count" / "$fleet_management_efficiency_rate_denominator" ))"
+        total_gross_income="$(( "$fleet_management_efficiency_rate_numerator" * "$income" * "$used_car_count" / "$fleet_management_efficiency_rate_denominator" ))"
     fi
-    (( money += "$total_income" ))
-    total_income_tax="$(( "$total_income" * "$income_tax_rate_numerator" / "$income_tax_rate_denominator" ))"
-    (( money -= "$total_income_tax" ))
+    echo "$total_gross_income"
+}
 
+handle_day () {
+    total_gross_income="$(get_total_gross_income)"
+    total_income_tax="$(( "$total_gross_income" * "$income_tax_rate_numerator" / "$income_tax_rate_denominator" ))"
+    total_net_income="$(( "$total_gross_income" - "$total_income_tax" ))"
     interest="$(( "$loan" * "$interest_rate_numerator" / "$interest_rate_denominator" ))"
-    if [[ "$money" -lt "$interest" ]]
-    then
-        echo "Money $money, cannot pay the interest $interest, business closed"
-        exit
-    else (( money -= "$interest" ))
-    fi
-
     total_car_rent_charge="$(( "$car_count" * "$car_rent_charge" ))"
-    if [[ "$money" -lt "$total_car_rent_charge" ]]
-    then
-        echo "Money $money, cannot pay the total car rent charge $total_car_rent_charge, business closed"
-        exit
-    else (( money -= "$total_car_rent_charge" ))
-    fi
-
     total_salary="$(( "$salary" * ("$driver_count" + "$manager_count") ))"
-    if [[ "$money" -lt "$total_salary" ]]
+    total_expenses="$(( "$total_car_rent_charge" + "$total_salary" ))"
+    day_result="$(( "$total_net_income" - "$total_expenses" ))"
+
+    if [[ "$(( "$day_result" + "$money" ))" < 0 ]]
     then
-        echo "Money $money, cannot pay the total salary $total_salary, business closed"
-        exit
-    else (( money -= "$total_salary" ))
+        # Notice that we don't add the day result to money here, but only if the check fails. It prevents to save the current, unfortunate state of the business. This in turn gives the user a small chance to take remedial action in the first day, by starting the simulation anew with the file with the current save.
+        echo "Money $money, day result $day_result, balance is going to be negative, business closed"
+        exit 0
+    else (( money += "$day_result" ))
     fi
 
     (( ++day ))
